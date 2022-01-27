8f8377020e9d19ce2669fae265cae65b9d1efa7b
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-01-27 14:59:40 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        names.splice(i, 1, `Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return names;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+};
