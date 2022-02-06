c54573e1c6172868b048865c8f3d75ed25628e53
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-02-06 15:10:15 -0600

    commit tree f6ae12dfa3940f64778ae3f6ab8383eaa73a6a36

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const names = [];
-
 function writeCards(names, event) {
+    let thankYouCards = []
     for (let i = 0; i < names.length; i++) {
-        names.splice(i, 1, `Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        thankYouCards[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-    return names;
+    return thankYouCards;
 }
-writeCards(["Charlie", "Samip", "Ali"], "birthday");
 
 function countDown() {
     let countDown = 10;
     while (countDown >= 0) {
         console.log(countDown--)
     }
-};
+}
